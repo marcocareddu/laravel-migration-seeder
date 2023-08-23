e5e41e4ca0528e2849864a4c1e79b704370160e2
marcocareddu <saldumaccu@tuta.io>
2023-08-23 17:51:46 +0200

    Added Train Seeder

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        
+    $new_train = new Train();
+    $train->company = $faker->company();
+    $train->train_number = $faker->postcode();
+    $train->departure_st = $faker->city();
+    $train->arrival_st = $faker->city();
+    $train->dep_hour = $faker->time('H_i');
+    $train->arr_hour = $faker->time('H_i');
+    $train->carriages = $faker->randomDigitNot(2);
+    $train->delay = $faker->randomDigitNot(2);
+    $train->deleted = $faker->boolean();
+
+    $train->save();
+    }
+}
