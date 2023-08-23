f086b2c2e6426ea54649f7621a7c89076402ad60
marcocareddu <saldumaccu@tuta.io>
2023-08-23 18:02:24 +0200

    Fixed typo

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
-use Faker\Generator as Faker;
+use Faker\Generator as FakerGenerator;
 
 class TrainSeeder extends Seeder
 {
