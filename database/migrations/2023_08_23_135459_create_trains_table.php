8622a51ea6fedae212d3733ac90759d4735687c6
marcocareddu <saldumaccu@tuta.io>
2023-08-23 16:28:41 +0200

    Created train  migration

--- database/migrations/2023_08_23_135459_create_trains_table.php
+++ database/migrations/2023_08_23_135459_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->smallInteger('train_number');
+            $table->string('departure_st');
+            $table->string('arrival_st');
+            $table->date('dep_hour');
+            $table->date('arr_hour');
+            $table->tinyInteger('carriages');
+            $table->date('delay');
+            $table->boolean('deleted')->default(1);
             $table->timestamps();
         });
     }
