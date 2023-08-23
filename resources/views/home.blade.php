af9415d2c3444fc80fd38f37e287e61fe7a53692
marcocareddu <saldumaccu@tuta.io>
2023-08-23 19:03:50 +0200

    Dispaly DB in table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,45 @@
 
 {{-- Main --}}
 @section('main')
-    <h1>Titolo di prova</h1>
+    <div class="container">
+        <h1 class="text-center mb-4">Orari Treni</h1>
 
-    @foreach ($trains as $train)
-        <h1>{{ $train->company }}</h1>
-    @endforeach
+        <h1></h1>
+
+
+
+        {{-- Table --}}
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Compagnia</th>
+                    <th scope="col">Numero</th>
+                    <th scope="col">Partenza</th>
+                    <th scope="col">Arrivo</th>
+                    <th scope="col">Ora partenza</th>
+                    <th scope="col">Ora arrivo</th>
+                    <th scope="col">Carrozze</th>
+                    <th scope="col">Ritardo</th>
+                </tr>
+            </thead>
+
+            {{-- Table inject here --}}
+            @foreach ($trains as $train)
+                <tbody>
+                    <tr>
+                        <th>{{ $train->company }}</th>
+                        <th>{{ $train->train_number }}</th>
+                        <th>{{ $train->departure_st }}</th>
+                        <th>{{ $train->arrival_st }}</th>
+                        <th>{{ $train->dep_hour }}</th>
+                        <th>{{ $train->arr_hour }}</th>
+                        <th>{{ $train->carriages }}</th>
+                        <th>{{ $train->delay }}</th>
+                    </tr>
+                </tbody>
+            @endforeach
+        </table>
+
+    </div>
 @endsection
